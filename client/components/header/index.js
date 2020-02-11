d97e58b0d5019328f0314696be37448e90c1febc
Victor Teixeira <victor.teixeira@uol.com.br>
2020-02-10 23:41:52 -0300

    Add Sass & estrutura de Header

--- client/components/header/index.js
+++ client/components/header/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './style.scss';
 
 export default () => {
     return (
-        <header>
-            HEADER
+        <header className="header">
+            <div className="header__container">
+                <a className="header__container__logo" href="/">Mercado Livre</a>
+                <input type="text"/>
+                <button>Buscar</button>
+            </div>
         </header>
     )
 }
