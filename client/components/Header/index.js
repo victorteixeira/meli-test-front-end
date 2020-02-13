570220363458e208e5217271b998f7cd524f80d8
Victor Teixeira <victor.teixeira@uol.com.br>
2020-02-13 01:17:09 -0300

    Bug CSS com cache

--- client/components/Header/index.js
+++ client/components/Header/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import './style.scss';
+import Head from 'next/head';
 
 const Header = props => {
     const router = useRouter();
@@ -23,6 +24,11 @@ const Header = props => {
 
     return (
         <header className="header">
+            <Head>
+                {process.env.NODE_ENV !== 'production' && (
+                    <link rel="stylesheet" href={'/_next/static/css/styles.chunk.css?v=' + Date.now()}/>
+                )}
+            </Head>
             <div className="header__container">
                 <Link href="/">
                     <a href="/" className="header__container__logo" title="Mercado Livre">Mercado Livre</a>
